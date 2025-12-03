f30ef10cb30766fd1a619b8d70791a040e45cd9f
vukmilenkovic <vukmilenkovic9@gmail.com>
2025-12-03 10:11:43 +0100

    Added a couplke of event listeners to change the background color of sqwuares when a mouse passes thru.

--- index.js
+++ index.js
@@ -3,16 +3,33 @@ container.style.display = 'flex';
 container.style.flexWrap = 'wrap';
 
 
-
+// TODO: Make into a function that creates squares n times
+// function createSquare(n); - replace the number of loops with the n arg
 // Create 16 divs contained in the container div 
 for (let i = 0; i < 16; i++){
     const grid = document.createElement("div");
+    grid.className = 'square';
 
     // Style the squares
     grid.style.display = 'flex';
     grid.style.flex = '0 0 25%';
     grid.style.height = '150px';
-    grid.style.outline = '1px solid black';
+    grid.style.outline = '1px solid black'
 
     container.appendChild(grid);
 }
+
+container.addEventListener("mouseover", (e) => {
+  if (e.target.classList.contains("square")) {
+    e.target.style.backgroundColor = 'red';
+  }
+
+});
+
+
+container.addEventListener('mouseout', (e) => {
+    if(e.target.classList.contains('square')){
+        e.target.style.backgroundColor = '';
+    }
+})
+
